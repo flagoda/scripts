3b3df9c6b7cfb95962034e60bb7c6caca083f942
florentlagoda <florent.lagoda@gmail.com>
2018-03-02 23:56:42 +0100

    Create disableTotalHibernation.bat

---
+++ disableTotalHibernation.bat
@@ -0,0 +1,6 @@
+REM Disable Hibernation
+powercfg -change -disk-timeout-ac 0
+powercfg -change -disk-timeout-dc 0
+powercfg -change -standby-timeout-ac 0
+powercfg -change -standby-timeout-dc 0
+powercfg -h off
